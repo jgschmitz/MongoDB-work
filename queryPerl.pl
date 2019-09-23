a0eb740250f0e266409d2c93de198714b263d67d
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2019-09-23 18:14:15 -0500

    Create queryPerl.pl

---
+++ queryPerl.pl
@@ -0,0 +1,17 @@
+open(my $fh, '<', "$file_path") or return 0;
+    while (my $line = <$fh>) {
+        chomp($line);
+        my ($key,$group,$value,$version,$file,$count) = split(/,/,$line);
+    my $key = 1;
+    foreach my $k (@{$aref}) {
+        if ($k->{"group"} eq $group) {
+        $k->{"values"} = [ sort uniq $value, @{$k->{"values"}} ];
+        $key = 0;
+        }
+    }
+        push @{ $aref }, {
+            group => $group,
+            values => [ $value ]
+        } if ($key);
+    }
+    close ($fh);
