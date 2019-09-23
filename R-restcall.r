5c5e973e5a2e0b0a3c8e60785304d929d09c56b4
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2019-09-23 18:45:33 -0500

    Create R-restcall.r

---
+++ R-restcall.r
@@ -0,0 +1,10 @@
+df= data.frame("col1" = c(seq(1:10)), "col2"=c(seq(11:20)))
+mydf = toJSON(df)
+ 
+req <- new_handle() %>%
+   handle_setopt("customrequest"="GET", postfields = mydf) %>%
+   handle_setheaders("Content-Type"= "application/json", "Cache-Control"="no-cache") %>%
+   curl_fetch_memory(url = "http://IP address/getdataframe")
+ 
+jsonlite::prettify(rawToChar(req$content))
+ 
