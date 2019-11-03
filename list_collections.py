592bc76ee33d46004b155d64d48596585d2f1ac4
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2019-11-03 08:17:56 -0600

    Create list_collections.py

---
+++ list_collections.py
@@ -0,0 +1,11 @@
+list_collections.py
+#!/usr/bin/python3
+
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017/')
+
+with client:
+    
+    db = client.testdb
+    print(db.collection_names())
