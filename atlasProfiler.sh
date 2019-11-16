4861a7a50aced54c24d7e25cb2a5bec82fe5302e
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2019-11-16 08:44:08 -0600

    Update atlasProfiler.sh

--- atlasProfiler.sh
+++ atlasProfiler.sh
@@ -1,4 +1,4 @@
-#test script to run against Atlas so showcase the profiler capabilities in Atlas
+****test script to run against Atlas so showcase the profiler capabilities in Atlas
 /usr/bin/sh
 
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]
