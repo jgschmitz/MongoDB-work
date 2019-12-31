2e50d6b786e7c9728350376f24e4e111af148006
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2019-12-31 10:03:04 -0600

    Update R-restcall.r

--- R-restcall.r
+++ R-restcall.r
@@ -1,4 +1,4 @@
-#restAPI call using R and other stuff that we want
+#restAPI call using R and other stuff that we want to see
 
 df= data.frame("col1" = c(seq(1:10)), "col3"=c(seq(11:21)))
 mydf = toJSON(df)
