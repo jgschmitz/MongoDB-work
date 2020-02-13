f26952ad0c7ff5e9f05dead6c5c92283f0b56cbe
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-02-13 08:06:23 -0600

    Create GoodJson.py

---
+++ GoodJson.py
@@ -0,0 +1,35 @@
+import pymongo
+import pprint
+import os
+import urllib
+import json
+import mongoengine_goodjson as gj
+import bson
+
+from datetime import datetime as dt
+from bson import json_util, ObjectId
+from bson.json_util import dumps, RELAXED_JSON_OPTIONS, STRICT_JSON_OPTIONS
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+host = "xxxhost"
+port = 27017
+dbname = "xxxdb"
+authdb = "admin"
+username = urllib.parse.quote("root")
+password = urllib.parse.quote("xxxpass")
+
+
+def getdata(username, password, host, port, dbname, authdb, tablenamelist):
+    connection_string = 'mongodb://{}:{}@{}:{}/{}?authSource={}'.format(username, password, host, port, dbname, authdb)
+    myclient = pymongo.MongoClient(connection_string)
+    mydb = myclient[dbname]
+    for elem in mydb.list_collection_names():
+        mycols = mydb[elem]
+        print('tablename:', elem)
+        with open(dir_path+'/'+elem+'.json','w') as f:
+            dump = dumps([doc for doc in mycols.find({},{ "_id": 0})], sort_keys=False, indent=4, default=json_util.default, json_options=RELAXED_JSON_OPTIONS)
+            f.write(dump)
+        f.close()
+
+getdata(username, password, host, port, dbname, authdb, tablenames)
