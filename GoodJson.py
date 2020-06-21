0c76050fe3d56b0d55a46f3df356ae74ef0626d5
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-06-21 17:46:16 -0500

    Update GoodJson.py

--- GoodJson.py
+++ GoodJson.py
@@ -6,6 +6,8 @@ import json
 import mongoengine_goodjson as gj
 import bson
 
+print "1,2,3,4,5,6,7,8,9,10,11"
+
 from datetime import datetime as dt
 from bson import json_util, ObjectId
 from bson.json_util import dumps, RELAXED_JSON_OPTIONS, STRICT_JSON_OPTIONS
