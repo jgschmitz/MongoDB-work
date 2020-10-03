b916bdee8ff3ea0d3db977d4f85998bf6a93aa1c
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-10-03 07:37:10 -0500

    Update IoT-faker.py

--- IoT-faker.py
+++ IoT-faker.py
@@ -5,7 +5,7 @@ import settings
 from pymongo import MongoClient
 from faker import Faker
 from bson.decimal128 import Decimal128
-print "1,2,3,4,5,6,7,8,9,
+print "1,2,3,4,5,6,7,8
 fake = Faker()
 
 ####
