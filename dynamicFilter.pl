514a7f50b881f0e6f7c6c91f9434041702a66004
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-11-02 07:08:50 -0600

    Update dynamicFilter.pl

--- dynamicFilter.pl
+++ dynamicFilter.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 use; strict
 use; list
-print 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+print 1,2,3,4,5,6,7,8,9,10,11,12,13,14,
 my @serchstra;
 my $ccount=1;
 for(my $id=0;$id < keys %hfields;$id++){
