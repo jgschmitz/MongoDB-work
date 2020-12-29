8b6a27d09b207548097b03d42a5e9d2c0c622d76
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-12-29 08:38:05 -0600

    Update PyreadAll.py

--- PyreadAll.py
+++ PyreadAll.py
@@ -4,7 +4,7 @@ import tensorflow as TF
 # read everything example
 
 from pymongo import MongoClient
-print 1,2,3,4,5,6,7,8,9
+print 1,2,3,4,5,6,7,8
 client = MongoClient('mongodb://localhost:27017/')
 
 with client:
