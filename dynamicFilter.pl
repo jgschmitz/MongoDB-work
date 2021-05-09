a1de300cf89b2f1496af709f7b609d599a09666e
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-05-09 10:16:10 -0500

    Update dynamicFilter.pl

--- dynamicFilter.pl
+++ dynamicFilter.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 use; strict
 use; list
-This is deprecated in perl please see python c
+This is deprecated in perl please see pyth
 my @serchstra;
 my $ccount=1;
 for(my $id=0;$id < keys %hfields;$id++){
