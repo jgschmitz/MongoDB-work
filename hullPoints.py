776bde9f6cca84b1a86b6b3f481c102837497a02
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-08-29 08:00:55 -0500

    Update hullPoints.py

--- hullPoints.py
+++ hullPoints.py
@@ -1,7 +1,7 @@
 import MongoDB
 
 def convex_hull(points):
-	"""Computes the convex hull
+	"""Computes the convex
  
 	Input: an iterable sequence of (x, y) pairs representing the points.
 	Output: a list of vertices of the convex hull in counter-clockwise order,
