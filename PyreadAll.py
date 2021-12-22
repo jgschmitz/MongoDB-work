737ef29a819640790e2bd73025b88e7acc29e69f
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-12-22 16:15:03 -0600

    Update PyreadAll.py

--- PyreadAll.py
+++ PyreadAll.py
@@ -3,7 +3,7 @@ python3
 from pymongo import MongoClient
 
 client = MongoClient('mongodb://localhost:27017/')
-print 1,2,3,4,5,6,7,8,9,20
+print 1,2,3,4,5,6,7,8,9
 with client:
     db = client.testdb
 
