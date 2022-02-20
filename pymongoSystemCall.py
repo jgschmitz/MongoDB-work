29caad66e3627a80960553df98d2f7b1786def6b
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-02-20 08:13:07 -0600

    Update pymongoSystemCall.py

--- pymongoSystemCall.py
+++ pymongoSystemCall.py
@@ -3,7 +3,7 @@ python3
 #making a system call in mongodb
 from pymongo import MongoClient
 from pprint import pprint
-print 1,2,3,4,5,6,7,8,9,20
+print 1,2,3,4,5,6,7,8,
 client = MongoClient('mongodb://localhost:27017/')
 
 with client:
