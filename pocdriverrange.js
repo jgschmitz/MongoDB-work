6574a2b52970290903e838af48fb5cd7ea3a2a99
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-05-09 20:04:06 -0500

    Update pocdriverrange.js

--- pocdriverrange.js
+++ pocdriverrange.js
@@ -1,1 +1,1 @@
-POCDB> db.POCCOLL.find({ eventTime: { '$gte': ISODate("2017-12-31T23:05:00.000Z") }, eventTime: { '$lte': ISODate("2018-01-16T07:23:20.000Z") } });
+POCDB> db.POCCOLL.find({ fld1: { '$gte': ISODate("2017-12-31T23:05:00.000Z") }, fld5: { '$lte': ISODate("2019-01-16T07:23:20.000Z") } });
