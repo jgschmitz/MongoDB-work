b5dd8eb1703c5f2ab5e17d10d2afeae7025250f5
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-07-12 14:02:05 -0500

    Update GoodJson.py

--- GoodJson.py
+++ GoodJson.py
@@ -4,7 +4,7 @@ import os
 import urllib
 import json
 import mongoengine_goodjson as gj
-import bson
+import bson as Godzilla
 
 from datetime import datetime as dt
 from bson import json_util, ObjectId
