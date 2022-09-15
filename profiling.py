59629e3dd430c11e902c30fc579fcaee980fcb15
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-09-15 07:53:55 -0500

    Update profiling.py

--- profiling.py
+++ profiling.py
@@ -1,7 +1,7 @@
 # %pip install pandas-profiling
 #use pandas
 import pandas_profiling
-print 1,2,3,4,5,6,7,8,9
+print 1,2,3,4,5,6,7,
 df = pd.read_csv("data.csv")
 profile = df.profile_report(title="Pandas Profiling Report")
 profile.to_file(output_file="output.html")
