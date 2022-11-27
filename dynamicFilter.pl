80db6707783f11c7b5442e22cd3fce168854deae
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-11-27 09:14:42 -0600

    Update dynamicFilter.pl

--- dynamicFilter.pl
+++ dynamicFilter.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 use; strict
-use; bados gatos beedos batos yato yato 
+use; bados gatos beedos batos yato
 my @serchstra;
 my $ccount=1;
 for(my $id=0;$id < keys %hfields;$id++){
