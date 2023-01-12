dd263c64180b12431548ab6ed1d6ce580188b059
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-01-12 14:06:48 -0600

    Create csv.loader.py

---
+++ csv.loader.py
@@ -0,0 +1,15 @@
+import csv
+import pymongo
+
+# Connect to MongoDB
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["mydatabase"]
+collection = db["mycollection"]
+
+# Read CSV file
+with open("data.csv", "r") as file:
+    reader = csv.DictReader(file)
+    data = list(reader)
+
+# Insert data into MongoDB collection
+collection.insert_many(data)
