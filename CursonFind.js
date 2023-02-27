7f007c3d8efa99ceb4cdd5aa705076aa45547db9
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-02-27 12:06:32 -0600

    Create CursonFind.js

---
+++ CursonFind.js
@@ -0,0 +1,19 @@
+// Create a cursor
+var cursor = db.collection.find({}).batchSize(10);
+
+// Iterate over the cursor and store the cursor id
+var docs = [];
+while (cursor.hasNext()) {
+  docs.push(cursor.next());
+  if (docs.length === 20) {
+    // Store the cursor id after processing 20 documents
+    var cursorId = cursor.id;
+    break;
+  }
+}
+
+// Resume the cursor using the stored cursor id
+cursor = db.collection.find({}).batchSize(10).resumable({ id: cursorId });
+while (cursor.hasNext()) {
+  docs.push(cursor.next());
+}
