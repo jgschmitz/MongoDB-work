fa0cac91d542ce37aa306dbd2d4dbb1fab1c20a4
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-03-01 10:55:00 -0600

    Create PyGraphql.py

---
+++ PyGraphql.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
+
+# set up a MongoDB Atlas client
+client = MongoClient("<MONGODB_ATLAS_URI>")
+
+# set up a GraphQL client using the HTTP transport
+transport = RequestsHTTPTransport(url="<GRAPHQL_API_URL>")
+client = Client(transport=transport, fetch_schema_from_transport=True)
+
+# define your GraphQL query
+query = gql('''
+  query {
+    movies {
+      title
+      genre
+      releaseYear
+      director
+    }
+  }
+''')
+
+# execute the query and print the results
+result = client.execute(query)
+print(result)
