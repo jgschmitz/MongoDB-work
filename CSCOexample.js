0c7d593c865f38d97b25e2a3b8f1094a51d3d47c
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-03-08 12:16:56 -0600

    Create CSCOexample.js

---
+++ CSCOexample.js
@@ -0,0 +1,45 @@
+{
+    "name": "defect1",
+    "category": "Cisco",
+    "subcategory": {
+        "wired": {
+            "layer2": {
+                "switching": {
+                    "IOS": {
+                        "status": "EOL"
+                    }
+                }
+            }
+        }
+    }
+},
+{
+    "name": "defect2",
+    "category": "Cisco",
+    "subcategory": {
+        "wired": {
+            "layer2": {
+                "switching": {
+                    "IOS": {
+                        "status": "EOL"
+                    }
+                }
+            }
+        }
+    }
+},
+{
+    "name": "defect3",
+    "category": "Cisco",
+    "subcategory": {
+        "wired": {
+            "layer2": {
+                "switching": {
+                    "IOS": {
+                        "status": "EOL"
+                    }
+                }
+            }
+        }
+    }
+}
