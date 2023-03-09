9dc46e6e1ad314503475f8d2231b23afc6dab19e
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-03-09 14:05:01 -0600

    Create YowsaTrigger.js

---
+++ YowsaTrigger.js
@@ -0,0 +1,8 @@
+exports = async function(changeEvent) {
+  const doc = changeEvent.fullDocument;
+  const amp_pk = doc.AMP_pk; // Replace with the actual name of the AMP pk field
+  const realm_id = amp_pk + "_realm"; // Generate the realm_id based on the AMP pk
+  const updatedDoc = { $set: { realm_id } };
+  const collection = context.services.get("mongodb-atlas").db("mydatabase").collection("mycollection"); // Replace "mydatabase" and "mycollection" with your actual database and collection names
+  await collection.updateOne({ _id: doc._id }, updatedDoc);
+};
