0fbf12d9758045afdc3fa02260f3cf47f4be59d3
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-04-06 11:56:05 -0500

    Create BillTags2.js
    This code creates a CSV file named output.csv in the current directory and writes the projectId, projectName, clusterId, clusterName, and clusterLabels fields to it. The labelMap object is converted to an array of records before being written to the CSV file using the csvWriter.writeRecords method.

---
+++ BillTags2.js
@@ -0,0 +1,33 @@
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+exports = async function (org, apiPublicKey, apiPrivateKey) {
+    let labelMap = {};
+
+    // ... existing code ...
+
+    console.log("finished");
+
+    // Output to CSV
+    const csvWriter = createCsvWriter({
+        path: 'output.csv',
+        header: [
+            {id: 'projectId', title: 'Project ID'},
+            {id: 'projectName', title: 'Project Name'},
+            {id: 'clusterId', title: 'Cluster ID'},
+            {id: 'clusterName', title: 'Cluster Name'},
+            {id: 'clusterLabels', title: 'Cluster Labels'}
+        ]
+    });
+    
+    const records = [];
+
+    for (const [projectId, projectMap] of Object.entries(labelMap)) {
+        for (const [clusterName, clusterLabels] of Object.entries(projectMap)) {
+            records.push({projectId, projectName: projectMap[clusterName], clusterId: clusterLabels.id, clusterName, clusterLabels});
+        }
+    }
+
+    await csvWriter.writeRecords(records);
+    
+    return labelMap;
+};
