96afd31c89092552c7210a4c716794c0e78aec1c
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-04-14 11:58:07 -0500

    Create Federated.js
    Federated Query Example MongoDB

---
+++ Federated.js
@@ -0,0 +1,32 @@
+# #this queries the movie database in Atlas (first tier) and also the data in federated storage - 
+
+db.Collection0.aggregate([
+  {
+    $match: {
+      "bedrooms": 3,
+      "review_scores.review_scores_rating": { $gt: 79 }
+    }
+  },
+  {
+    $lookup: {
+      from: "movies",
+      let: { title: "$name" },
+      pipeline: [
+        {
+          $match: {
+            $expr: { $eq: ["$title", "$$title"] }
+          }
+        }
+      ],
+      as: "movies"
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      name: 1,
+      numProperties: { $count: "numProperties" },
+      movies: 1
+    }
+  }
+])
