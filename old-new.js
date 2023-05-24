3af7bf3e63208aa7b48fbe70d9d2fe3c19a937d8
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-05-24 12:35:50 -0500

    Update old-new.js

--- old-new.js
+++ old-new.js
@@ -1,40 +1,26 @@
 exports = async function(payload, response) {
-  // Get the changed document from the trigger event
-  const changedDocument = payload.fullDocument;
-  
-  // Connect to the "new_db" in the MongoDB Atlas cluster
+  const { operationType, fullDocument } = payload;
   const newDB = context.services.get("mongodb-atlas").db("new_db");
+  const targetCollection = newDB.collection("target_collection");
   
   try {
-    // Determine the type of operation (INSERT/UPDATE/DELETE)
-    const operationType = payload.operationType;
-    
-    if (operationType === "insert") {
-      // Handle INSERT operation
-      await newDB.collection("target_collection").insertOne(changedDocument);
-    } else if (operationType === "update") {
-      // Handle UPDATE operation
-      const updatedDocument = payload.updateDescription.updatedFields;
-      await newDB.collection("target_collection").updateOne({_id: changedDocument._id}, {$set: updatedDocument});
-    } else if (operationType === "delete") {
-      // Handle DELETE operation
-      await newDB.collection("target_collection").deleteOne({_id: changedDocument._id});
+    switch (operationType) {
+      case "insert":
+        await targetCollection.insertOne(fullDocument);
+        break;
+      case "update":
+        const { updatedFields } = payload.updateDescription;
+        await targetCollection.updateOne({ _id: fullDocument._id }, { $set: updatedFields });
+        break;
+      case "delete":
+        await targetCollection.deleteOne({ _id: fullDocument._id });
+        break;
     }
     
-    // Log a success message
     console.log(`Successfully processed trigger event: ${operationType}`);
-    
-    // Set the response status
-    response.setStatusCode(200);
-    response.setHeader("Content-Type", "application/json");
-    response.setBody({message: "Trigger event processed successfully"});
+    response.setStatusCode(200).setHeader("Content-Type", "application/json").setBody({ message: "Trigger event processed successfully" });
   } catch (error) {
-    // Log an error message
     console.error(`Error processing trigger event: ${operationType}`, error);
-    
-    // Set the response status
-    response.setStatusCode(500);
-    response.setHeader("Content-Type", "application/json");
-    response.setBody({message: "Error processing trigger event"});
+    response.setStatusCode(500).setHeader("Content-Type", "application/json").setBody({ message: "Error processing trigger event" });
   }
 };
