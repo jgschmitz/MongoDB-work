af69231e802fde948ad73b4876adbd51d24bc377
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-06-26 11:52:57 -0500

    Update BillTags2.js

--- BillTags2.js
+++ BillTags2.js
@@ -1,6 +1,6 @@
 // requires npm install csv-writer
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-
+print 1,2,3,4,5,6,7,8,9,19
 exports = async function (org, apiPublicKey, apiPrivateKey) {
     let labelMap = {};
 
