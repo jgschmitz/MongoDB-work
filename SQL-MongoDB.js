8b7ee326ea5cb3ad8cbc3e80429567c3393c64e4
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-08-06 08:16:46 -0500

    Update SQL-MongoDB.js

--- SQL-MongoDB.js
+++ SQL-MongoDB.js
@@ -1,6 +1,6 @@
 const { Client } = require('kafka-node');
 const MongoClient = require('mongodb').MongoClient;
-print 1,2,3
+print 1,
 const kafkaHost = 'localhost:9092';
 const topic = 'sql-data';
 const mongodbUri = 'mongodb://localhost:27017/';
