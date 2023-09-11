3ea5364ec5bdef824d4ab1080b2d0f6902f9274e
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-09-11 16:48:41 -0500

    Create PANW-Example.js
    It starts with the initial $match, $unwind, $match, and $lookup stages to filter and join the collections based on your criteria.
    
    After the $lookup, it adds another $match stage to filter documents where there's a match in matching_resource_code.
    
    The $group stage groups the documents by their original _id and counts the number of matched violations within each group.
    
    Finally, the $replaceRoot stage replaces the root document with the original document merged with the count field, effectively adding the count of matched violations to the original document.

---
+++ PANW-Example.js
@@ -0,0 +1,59 @@
+db.getCollection("bucketed").aggregate([
+    {
+        "$match": {
+            "repo.customer_name": "1069803756901857280",
+        },
+    },
+    {
+        "$unwind": {
+            "path": "$violations",
+        },
+    },
+    {
+        "$match": {
+            "violations.status": "OPEN",
+        },
+    },
+    {
+        "$lookup": {
+            "from": "source_code_coll",
+            "localField": "violations.asset.id",
+            "foreignField": "asset.id",
+            "as": "matching_resource_code",
+            "pipeline": [
+                {
+                    "$search": {
+                        "index": "default",
+                        "text": {
+                            "path": "asset.iac_resource.resource_code",
+                            "query": "health-check",
+                        },
+                    },
+                },
+                {
+                    "$project": {
+                        "_id": 0,
+                        "asset.iac_resource.resource_code": 1,
+                    },
+                },
+            ],
+        },
+    },
+    {
+        "$match": {
+            "matching_resource_code": { "$ne": [] },
+        },
+    },
+    {
+        "$group": {
+            "_id": "$_id", // Group by the original document's _id
+            count: { $sum: 1 }, // Count the matched violations
+            document: { $first: "$$ROOT" }, // Preserve the original document
+        },
+    },
+    {
+        "$replaceRoot": {
+            newRoot: { $mergeObjects: ["$document", { count: "$count" }] },
+        },
+    },
+]);
