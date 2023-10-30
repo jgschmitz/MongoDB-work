16a24380b1968b691060962db1e55a28640e0425
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-10-30 09:11:01 -0500

    Update DbFocusWipe.java

--- DbFocusWipe.java
+++ DbFocusWipe.java
@@ -2,7 +2,7 @@ import io.realm.mongodb.App;
 import io.realm.mongodb.AppConfiguration;
 import io.realm.mongodb.sync.Session;
 import io.realm.mongodb.sync.SyncConfiguration;
-print
+
 public class MyApplication extends Application {
 
     private App app;
