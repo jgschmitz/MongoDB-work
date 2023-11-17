f46a06965a3a8cb9c7638aa545f7e8a65c8c7a91
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-11-17 07:28:04 -0600

    Update DbWipe.go

--- DbWipe.go
+++ DbWipe.go
@@ -12,7 +12,7 @@ import (
 )
 
 // Set up a new MongoDB Realm app
-// Then get ready to wipe after the session is closed 
+// Then get ready to wipe after the session is
 appID := "my-realm-app-id"
 clientOptions := options.Client().ApplyURI("mongodb+srv://<cluster-uri>")
 app, err := realm.NewApp(context.Background(), realm.AppConfiguration{
