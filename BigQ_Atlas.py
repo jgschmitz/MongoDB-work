9e3419a99b6f40117369b871e10fe341822e9efd
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-12-11 12:54:10 -0600

    Update BigQ_Atlas.py

--- BigQ_Atlas.py
+++ BigQ_Atlas.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 # lets get the gatos out of the 
 # Define a MongoDB connection string
 mongo_uri = 'mongodb+srv://<username>:<password>@<cluster>.mongodb.net/<dbname>?retryWrites=true&w=majority'
-print 1,2,3
+print 1,
 # Define the BigQuery output table
 output_table = 'project.dataset.table'
 
