4b5b31d876751925bd6ff497954d4a900754ad28
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-12-25 08:26:28 -0600

    Update BigQ_Atlas.py

--- BigQ_Atlas.py
+++ BigQ_Atlas.py
@@ -1,4 +1,4 @@
-#this mugg uses bean and lamba to read and
+#this mugg uses bean and lamba to 
 import apache_beam as beam
 from apache_beam.io.gcp.bigquery import WriteToBigQuery
 from pymongo import MongoClient
