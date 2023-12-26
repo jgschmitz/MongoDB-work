be8b3edf70aac43ce902d8905d7c31194bec5a58
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-12-26 15:31:58 -0600

    Update FEDeratedGraph.js

--- FEDeratedGraph.js
+++ FEDeratedGraph.js
@@ -1,7 +1,7 @@
 const { ApolloServer } = require('apollo-server');
 const { buildFederatedSchema } = require('@apollo/federation');
 const { makeExecutableSchema } = require('graphql-tools');
-print 1,2,
+print 1
 // Define the first schema
 const usersSchema = makeExecutableSchema({
   typeDefs: `
