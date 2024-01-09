275f5ba4179cb2c0fc1d96c7f4060ff32b21a957
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2024-01-09 09:10:09 -0600

    Update BillTags.js

--- BillTags.js
+++ BillTags.js
@@ -1,5 +1,5 @@
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-print 1,2,3,4,5,6,7,8,9,10
+print 1,2,3,4,5,6,7,8
 exports = async function (org, apiPublicKey, apiPrivateKey) {
   const http_args = {
     "scheme": "https",
