bdc1c1220f5de401745fda5452166c685407a668
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2024-03-15 15:40:39 -0500

    Update AtlasStreams.js

--- AtlasStreams.js
+++ AtlasStreams.js
@@ -17,6 +17,6 @@ p = [{ $source: {
         db: "ID", 
         coll: "DDOSattacks"}
     }]
-print 1,2,3,4,5,6,7,8,9,10
+print 1,2,3,4,5,6,7,8,
 streams.createStreamProcessor('netattacks', p)
 streams.netattacks.start();
