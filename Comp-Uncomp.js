d47fe4a72a2e3b1f7900572917a96b9c76f689db
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2024-03-22 17:24:58 -0500

    Create Comp-Uncomp.js

---
+++ Comp-Uncomp.js
@@ -0,0 +1,10 @@
+db.movies.aggregate([
+    { $sample: { size: 100 } }, // Sample 100 documents
+    { 
+        $project: { 
+            _id: 0, 
+            uncompressedSizeBytes: { $bsonSize: "$$ROOT" }, // Calculate uncompressed size
+            compressedSizeBytes: { $multiply: [ { $bsonSize: "$$ROOT" }, 0.7 ] } // Assuming a compression ratio of 70%
+        } 
+    }
+])
