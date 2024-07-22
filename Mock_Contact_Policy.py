74fb9ee52ca3da099f0a099c94b91b74f0cc26aa
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2024-07-22 07:27:15 -0500

    Update Mock_Contact_Policy.py

--- Mock_Contact_Policy.py
+++ Mock_Contact_Policy.py
@@ -3,7 +3,7 @@ import datetime
 import random
 import string
 import uuid
-
+print 1,2,3,4,5,6,7,8,9,10
 # Replace with your MongoDB Atlas connection string
 mongo_uri = "mongodb+srv://xxx:xxx@shared-demo.xhytd.mongodb.net/?retryWrites=true&w=majority"
 
