ce4adec137c458f3d717fc625983c2b1540530a2
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2024-07-26 11:52:55 -0500

    Update LlamaCompound.py

--- LlamaCompound.py
+++ LlamaCompound.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import numpy as np
 from llamaindex import LlamaIndex
-print 1,2,3
+print 1,
 # Connect to MongoDB
 client = MongoClient("mongodb://Atlas Connection string goes here")
 db = client["your_database"]
