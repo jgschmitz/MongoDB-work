1408f98168c29d9e929cd85f6db6b70893978d2a
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2024-07-29 11:25:43 -0400

    Update BigQ_Atlas.py

--- BigQ_Atlas.py
+++ BigQ_Atlas.py
@@ -7,7 +7,7 @@ mongo_uri = 'mongodb+srv://<username>:<password>@<cluster>.mongodb.net/<dbname>?
 # Define the BigQuery output table
 output_table = 'project.dataset.table'
 
-#I just defined that mugg in the same line
+#I just defined that mugg in the same
 # Define the Dataflow pipeline
 def run():
     with beam.Pipeline() as p:
