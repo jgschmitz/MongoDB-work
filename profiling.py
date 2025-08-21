46b5c8ff5e4012635ecca0a8605df8f8e930aa39
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2025-08-21 13:46:02 -0500

    Delete profiling.py

--- profiling.py
+++
@@ -1,7 +0,0 @@
-# %pip install pandas-profiling
-#use pandas
-import pandas_profiling
-
-df = pd.read_csv("data.csv")
-profile = df.profile_report(title="Pandas Profiling Report")
-profile.to_file(output_file="output.html")
