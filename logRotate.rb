9c18a2475f83b4d1cf7124bbcc7e719ce2f52d0e
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2025-11-10 12:44:36 -0600

    Delete logRotate.rb

--- logRotate.rb
+++
@@ -1,9 +0,0 @@
-include_recipe "mongo-bootstrap"
-
-# run on your own this: logrotate -vf /etc/logrotate.d/mongodb
-#
-logrotate "mongodb" do
-  paths "#{node[:mongodb][:logpath]}/*.log"
-  period node[:mongodb][:logrotate][:period]
-  keep node[:mongodb][:logrotate][:keep]
-end
